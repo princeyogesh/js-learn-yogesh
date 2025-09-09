90c3d0f981b7f3b309ea6010e385d20ce8b6b79b
princeyogesh <yogeshdeshpande79@gmail.com>
2025-09-09 10:09:31 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accoutnId = 14453
+let accountemial = "yogesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accoutnId = 2 //not allowed 
+accountemial = "hc@hc.com"
+accountPassword = "6789"
+accountCity = "BLR"
+console.log(accoutnId);
+/*
+        PREFER not to use var 
+        because of issue on block scope and functional scope
+
+*/ 
+console.table([accoutnId, accountemial, accountPassword, accountCity, accountState]);
+
+
