3b2357ebdbf2633198b511a6118f36e750126b94
princeyogesh <yogeshdeshpande79@gmail.com>
2025-09-09 04:57:32 +0000

    js chai 03 added

---
+++ 01_basics/03_datatypeConversionOperations.js
@@ -0,0 +1,58 @@
+let someNumber = 33;
+let strinNumber = String(someNumber);
+//console.log(strinNumber);
+//console.log(typeof(strinNumber));
+
+/***********OPERATIONS********************** */
+let value = 3;
+let negValiue = -value;
+//console.log(negValiue);
+
+/*
+    console.log(2+2);
+    console.log(2-2);
+    console.log(2*2);
+    console.log(2**3);
+    console.log(2%3);
+*/
+
+let str1 = "Hello ";
+let str2 = "Yogesh";
+console.log(str1 + str2);
+
+
+console.log("1"  + 2);      //automatically converts number to string
+console.log(1 + "2");
+console.log("1" + 2 + 2);   ///---122
+console.log(1 + 2 + "2");   ////---32 HOWWWWWWWWWW?
+
+/*
+
+ */
+console.log(true);
+console.log(+true);
+//console.log(true+); //error
+console.log(+"");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2
+console.log(num1);
+
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+console.log(gameCounter++);
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
